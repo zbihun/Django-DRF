2720f79975950411133039d92e5a716e2993fc1b
zbihun <bighun.zoryana@gmail.com>
2023-08-19 17:37:19 +0300

    Created first product models, apis and tests

---
+++ ecommerce/ecommerce/tests/factories.py
@@ -0,0 +1,28 @@
+import factory
+
+from ecommerce.product.models import Brand, Category, Product
+
+
+class CategoryFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Category
+
+    name = factory.Sequence(lambda n: f"Category_{n}")
+
+
+class BrandFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Brand
+
+    name = factory.Sequence(lambda n: f"Brand_{n}")
+
+
+class ProductFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Product
+
+    name = "test_product"
+    description = "test_description"
+    is_digital = True
+    brand = factory.SubFactory(BrandFactory)
+    category = factory.SubFactory(CategoryFactory)
