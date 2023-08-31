0966af247f631c3edcc5dd4b8b6b3f62240fcccd
zbihun <bighun.zoryana@gmail.com>
2023-08-31 21:43:08 +0300

    Second part

--- ecommerce/ecommerce/product/views.py
+++ ecommerce/ecommerce/product/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.db import connection
 from drf_spectacular.utils import extend_schema
+from pygments import highlight
+from pygments.formatters import TerminalFormatter
+from pygments.lexers import SqlLexer
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
+from sqlparse import format
 
 from .models import Brand, Category, Product
 from .serializers import BrandSerializer, CategorySerializer, ProductSerializer
@@ -38,9 +43,32 @@ class ProductViewSet(viewsets.ViewSet):
     A simple viewset for viewing all Products
     """
 
-    queryset = Product.objects.all()
+    queryset = Product.objects.isactive().all()
+    lookup_field = "slug"
+
+    def retrieve(self, request, slug=None):
+        serializer = ProductSerializer(
+            self.queryset.filter(slug=slug).select_related("category", "brand"),
+            many=True,
+        )
+        data = Response(serializer.data)
+        return data
 
     @extend_schema(responses=ProductSerializer)
     def list(self, request):
         serializer = ProductSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+    @action(
+        methods=["get"],
+        detail=False,
+        url_path=r"category/(?P<slug>[\w-]+)/all",
+    )
+    def list_product_by_category_slug(self, request, slug=None):
+        """
+        An endpoint to return products by category
+        """
+        serializer = ProductSerializer(
+            self.queryset.filter(category__slug=slug), many=True
+        )
         return Response(serializer.data)
--- ecommerce/ecommerce/tests/product/test_models.py
+++ ecommerce/ecommerce/tests/product/test_models.py
@@ -1,4 +1,5 @@
 import pytest
+from django.core.exceptions import ValidationError
 
 pytestmark = pytest.mark.django_db
 
@@ -19,3 +20,15 @@ class TestProductModel:
     def test_str_method(self, product_factory):
         data = product_factory(name="test_product")
         assert data.__str__() == "test_product"
+
+
+class TestProductLineModel:
+    def test_str_method(self, product_line_factory):
+        data = product_line_factory(sku="12345")
+        assert data.__str__() == "12345"
+
+    def test_duplicate_order_values(self, product_line_factory, product_factory):
+        product = product_factory()
+        product_line_factory(product=product, order=1)
+        with pytest.raises(ValidationError):
+            product_line_factory(product=product, order=1).clean()
