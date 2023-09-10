96bd8f03bdc503852b897c60b9cba47375bf4f34
zbihun <bighun.zoryana@gmail.com>
2023-09-10 22:03:55 +0300

    DEV 1.0.7

--- ecommerce/ecommerce/product/views.py
+++ ecommerce/ecommerce/product/views.py
@@ -1,13 +1,8 @@
-from django.db import connection
 from django.db.models import Prefetch
 from drf_spectacular.utils import extend_schema
-from pygments import highlight
-from pygments.formatters import TerminalFormatter
-from pygments.lexers import SqlLexer
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from sqlparse import format
 
 from .models import Category, Product, ProductImage, ProductLine
 from .serializers import (
@@ -40,12 +35,12 @@ class ProductViewSet(viewsets.ViewSet):
 
     def retrieve(self, request, slug=None):
         serializer = ProductSerializer(
-            self.queryset.filter(slug=slug),
-            # .prefetch_related(
-            #     Prefetch("attribute_values__attribute")
-            # ),
-            # .prefetch_related("product_line__product_image")
-            # .prefetch_related("product_line__attribute_values__attribute"),
+            self.queryset.filter(slug=slug)
+            .prefetch_related(
+                Prefetch("attribute_values__attribute")
+            )
+            .prefetch_related("product_line__product_image")
+            .prefetch_related("product_line__attribute_values__attribute"),
             many=True,
         )
         data = Response(serializer.data)
